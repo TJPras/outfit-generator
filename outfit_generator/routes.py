6abe9f081e3033c5279b3b27a8eefd498297436d
tjp9390 <tjp9390@g.rit.edu>
2024-12-22 20:54:52 -0500

    Renamed route and using new stylesheet.

--- outfit_generator/routes.py
+++ outfit_generator/routes.py
@@ -1,4 +1,4 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    config.add_route('outfit_display', '/display')
+    config.add_route('outfit_display', '/outfit_generator')
